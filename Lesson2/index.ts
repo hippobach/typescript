19cd68d88233e58acd4c4c7db94196d08b5c1b8e
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2023-12-14 23:28:09 +0700

    tyoe inference in ts

---
+++ Lesson2/index.ts
@@ -0,0 +1,9 @@
+let count: number = 10;
+// type string is not assignable to type number
+// count = 'bach';
+
+count = 12;
+console.log(count);
+
+let array = ['hippo', 'bach', 33];
+array.push(15);
