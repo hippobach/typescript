8c9ddb91ec462b2ead973dcf66a50a6abcd7b084
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2023-12-15 23:46:38 +0700

    type string in ts

---
+++ Lesson4/index.ts
@@ -0,0 +1,6 @@
+let name1: string = `Bach`;
+console.log(name1);
+let name2: string = ' Nguyen "Xuan"';
+console.log(name1.concat(name2));
+let name3: string = `${name1} from UTC`; // template string
+console.log(name3);
