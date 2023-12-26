54e4a02d737fec6a50aa47d39ccd50c2a1037097
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2023-12-26 15:40:56 +0700

    type void in typescript

---
+++ Lesson11/index.ts
@@ -0,0 +1,11 @@
+const sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+console.log(sum(3, 5));
+
+const handleLogs = (log: string): void => {
+  console.log('Xy ly log ', log);
+};
+
+handleLogs('Not Found - 404');
