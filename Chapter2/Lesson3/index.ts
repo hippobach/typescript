44aa52879533e063621b5ee8f02d44743e0081fd
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2024-01-09 11:04:54 +0700

    for loop in typescript

---
+++ Chapter2/Lesson3/index.ts
@@ -0,0 +1,10 @@
+for (let i = 0; i < 10; i++) {
+  console.log('i = ', i);
+}
+
+let words: string = 'HippoBach2003';
+let a: any;
+for (let i = 0; i < words.length; i++) {
+  a = words.charAt(i);
+  console.log(a);
+}
