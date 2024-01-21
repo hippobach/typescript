a8a3a863b2cafdd3341670728509d2ecdb66214e
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2024-01-21 17:12:49 +0700

    class in typescript

---
+++ Chapter4/Lesson1/index.ts
@@ -0,0 +1,19 @@
+class Person {
+  ssn: string;
+  firstName: string;
+  lastName: string;
+
+  constructor(ssn: string, firstName: string, lastName: string) {
+    this.ssn = ssn;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  getFullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+let student = new Person('aaa', 'Hippo', 'Bach');
+console.log(student.getFullName());
+console.log(student);
